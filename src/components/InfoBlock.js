45fdce1386f5c15099e12137ce84bf1816340611
olegbb1502 <bolobanoleg@gmail.com>
2019-06-05 10:00:28 +0300

    new

--- src/components/InfoBlock.js
+++ src/components/InfoBlock.js
@@ -1,5 +1,11 @@
 import React, {Component} from 'react';
 import firebase from './firebase';
+import Button from '@material-ui/core/Button';
+import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogContentText from '@material-ui/core/DialogContentText';
+import DialogTitle from '@material-ui/core/DialogTitle';
 
 class InfoBlock extends Component{
     constructor(){
@@ -12,6 +18,7 @@ class InfoBlock extends Component{
         }
 
         this.resetBoard = this.resetBoard.bind(this);
+        this.handleClose = this.handleClose.bind(this);
     }
 
     componentDidMount(){
@@ -23,6 +30,12 @@ class InfoBlock extends Component{
                reset: item.reset
            })
         });
+    }
+
+    handleClose() {
+        this.setState({
+            modalWindow: false
+        })
     }
 
     resetBoard(){
@@ -45,16 +58,37 @@ class InfoBlock extends Component{
                     <button className="settings">Settings</button>
                     <button className="reset" onClick={()=>{this.setState({modalWindow: true})}}>Reset</button>
                 </div>
-                <div className={modalWindow ? "modal open" : "modal"}>
-                    <div className="box">
-                        <h3>Restart system</h3>
-                        <p>After restarting system all data will deleted from database. When system launch it connects to WiFi automatically.</p>
-                        <div className="buttons">
-                            <button onClick={()=>{this.setState({modalWindow: false})}}>Cancel</button>
-                            <button onClick={this.resetBoard}>Reload board</button>
-                        </div>
-                    </div>
-                </div>
+                {/*<div className={modalWindow ? "modal open" : "modal"}>*/}
+                    {/*<div className="box">*/}
+                        {/*<h3>Restart system</h3>*/}
+                        {/*<p>After restarting system all data will deleted from database. When system launch it connects to WiFi automatically.</p>*/}
+                        {/*<div className="buttons">*/}
+                            {/*<button onClick={()=>{this.setState({modalWindow: false})}}>Cancel</button>*/}
+                            {/*<button onClick={this.resetBoard}>Reload board</button>*/}
+                        {/*</div>*/}
+                    {/*</div>*/}
+                {/*</div>*/}
+                <Dialog
+                    open={modalWindow}
+                    onClose={this.handleClose}
+                    aria-labelledby="alert-dialog-title"
+                    aria-describedby="alert-dialog-description"
+                >
+                    <DialogTitle id="alert-dialog-title">{"Restart system"}</DialogTitle>
+                    <DialogContent>
+                        <DialogContentText id="alert-dialog-description">
+                            After restarting system all data will deleted from database. When system launch it connects to WiFi automatically.
+                        </DialogContentText>
+                    </DialogContent>
+                    <DialogActions>
+                        <Button onClick={this.handleClose} color="primary">
+                            Cancel
+                        </Button>
+                        <Button onClick={this.resetBoard} variant="contained" color="secondary">
+                            Reload Board
+                        </Button>
+                    </DialogActions>
+                </Dialog>
             </div>
         );
     }
